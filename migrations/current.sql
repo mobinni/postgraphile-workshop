6c6b79d50c8e6682a486e0df793370ca00fb9d74
Mo Binni <mo@binni.io>
2023-06-27 15:01:42 -0400

    rls wip

--- migrations/current.sql
+++ migrations/current.sql
@@ -1,1 +1,25 @@
 -- Enter migration here
+GRANT USAGE ON SCHEMA "app_public" TO client;
+GRANT SELECT ON ALL TABLES IN SCHEMA "app_public" TO client;
+
+DROP POLICY IF EXISTS user_activities_permission ON "app_public".activities;
+DROP FUNCTION IF EXISTS "app_public".get_current_account_ids;
+
+CREATE OR REPLACE FUNCTION "app_public".get_current_account_ids ()
+    RETURNS text[]
+AS $$
+SELECT
+    STRING_TO_ARRAY(CURRENT_SETTING('user.account_ids', TRUE), ',');
+$$
+LANGUAGE sql
+STABLE;
+
+-- CREATE POLICY user_activities_permission ON "app_public".activities
+--     FOR SELECT
+--     USING ("account_id" = ANY("app_public".get_current_account_ids ()));
+
+
+DROP FUNCTION IF exists "app_public".activities_by_account;
+CREATE OR REPLACE FUNCTION activities_by_account(account_ids text[]) RETURNS SETOF "app_public".activities AS $$
+    SELECT * FROM "app_public".activities WHERE account_id = ANY(account_ids);
+$$ LANGUAGE SQL STABLE SECURITY DEFINER;
--- mock-insert.sql
+++ mock-insert.sql
@@ -1,44 +1,21 @@
 do
 $$
     begin
-        for r in 0..5000
+        for r in 0..10
             loop
                 insert into "app_public".activities(canonical_id,
                                                     account_id,
                                                     amount,
-                                                    type,
                                                     currency,
-                                                    occurred_at,
-                                                    p2p_handle)
-                values (gen_random_uuid(),
-                        gen_random_uuid(),
-                        random() * 100,
-                        'CAD',
-                        'p2p_payment',
-                        (select NOW() + (random() * (NOW() + '10 days' - NOW()))),
-                        '$mo');
-            end loop;
-    end;
-$$;
-
-do
-$$
-    begin
-        for r in 0..5000
-            loop
-                insert into "app_public".activities(canonical_id,
-                                                    account_id,
-                                                    amount,
                                                     type,
-                                                    currency,
                                                     occurred_at,
                                                     spend_merchant,
                                                     fx_rate)
                 values (gen_random_uuid(),
-                        gen_random_uuid(),
+                        'account-123',
                         random() * 100,
                         'CAD',
-                        'p2p_payment',
+                        'spend',
                         (select NOW() + (random() * (NOW() + '10 days' - NOW()))),
                         'costco',
                         1.36);
